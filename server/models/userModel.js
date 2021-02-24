0bc2c66de8b774cba3ccd1a9d24fc0b6fc379fc3
Rijon <rijon.r@yahoo.com>
2021-02-24 10:44:30 +0600

    create mongoose schema and model

---
+++ server/models/userModel.js
@@ -0,0 +1,12 @@
+const mongoose  = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    email : {type : String , required : true},
+    passwordHash : { type : String , required : true}
+})
+
+//singular user then mongosse will make users
+const User = mongoose.model("user" , userSchema);
+
+module.exports = User;
+
