9af45193e2ef030c94251788c2cf901feb1f3962
Rijon <rijon.r@yahoo.com>
2021-02-24 11:04:21 +0600

    register endpoint

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server started on port : ${PORT}`));
 
+app.use(express.json());
+
 //connect to mongodb
 
 mongoose.connect(
@@ -26,4 +28,9 @@ mongoose.connect(
       console.log("Connected to MongoDB");
     }
   );
+
+
+  // set up routes
+
+  app.use("/auth" , require("./routers/userRouter"));
 
---
+++ server/routers/userRouter.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+router.post("/", (req,res)=> {
+    try{
+        const { email , password , passwordVerify} = req.body;
+
+
+
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).send();
+    }
+
+})
+
+module.exports = router;
