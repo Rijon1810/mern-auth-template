2d47205685ee0a97e3d24b15cfc69c5443c828b1
Rijon <rijon.r@yahoo.com>
2021-02-24 10:36:09 +0600

    mongodb setup and connecting to database

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+
+//set up server
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, ()=> console.log(`Server started on port : ${PORT}`));
 
+//connect to mongodb
 
-app.get("/test" , (req,res)=> {
-    res.send("It works");
-});
-
+mongoose.connect(
+    process.env.MDB_CONNECT,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      
+    },
+    (err) => {
+      if (err) return console.error(err);
+      console.log("Connected to MongoDB");
+    }
+  );
 
